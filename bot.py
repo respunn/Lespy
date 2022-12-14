b6cf5f5488ea5f6b445cf644c0fb64b4eae78784
respuNN <68736280+respuNN@users.noreply.github.com>
2022-12-14 18:52:05 +0300

    on_ready

--- bot.py
+++ bot.py
@@ -28,62 +28,6 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-#Testing
-#Latency
-@bot.tree.command(name='latency', description="Shows bot's latency.")
-@app_commands.checks.has_permissions(administrator=True)
-async def latency(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Latency is {round(bot.latency * 1000)}ms", ephemeral=True)
-
-#Description
-@bot.tree.command(name='illusa', description="What is Illusa?")
-async def malderecesi(interaction: discord.Interaction):
-    await interaction.response.send_message(content=f'To be Announced.')
-
-#Avatar
-@bot.tree.command(name='avatar', description="You can get people's profile pictures.")
-async def avatar(interaction: discord.Interaction, member: Optional[discord.Member]):
-    if member == None:
-        ProfilePicture = interaction.user.display_avatar.url
-        await interaction.response.send_message(ProfilePicture)
-    else:
-        ProfilePicture = member.avatar.url
-        await interaction.response.send_message(ProfilePicture)
-
-#Clear
-@bot.tree.command(name='clear', description="Clears the given amount of messages.")
-@app_commands.checks.has_permissions(manage_messages=True)
-async def clear(interaction: discord.Interaction, amount: int):
-    await interaction.response.send_message(f"Successfully deleted {amount} of messages.", ephemeral=True)
-    await interaction.channel.purge(limit=amount)
-
-#Ban
-@bot.tree.command(name='ban', description="Bans members.")
-@app_commands.checks.has_permissions(ban_members=True)
-async def ban(interaction: discord.Interaction, member: discord.Member, delete_messages: int ,reason: str = "No reason provided."):
-    try:
-        await member.ban(reason = reason, delete_message_days = delete_messages)
-        await interaction.response.send_message(f"Successfully banned {member.mention} for {reason}")
-    except:
-        await interaction.response.send_message(f"The {member.mention} could not be banned from the server.", ephemeral=True)
-@ban.error
-async def ban_error(interaction: discord.Interaction, error):
-    if isinstance(error, MissingPermissions):
-        await interaction.response.send_message(f"You don't have permission to use this command!", ephemeral=True)
-
-#Kick
-@bot.tree.command(name='kick', description="Kicks members.")
-@app_commands.checks.has_permissions(kick_members=True)
-async def kick(interaction: discord.Interaction, member: discord.Member, reason: str = "No reason provided"):
-    try:
-        await member.kick(reason = reason)
-        await interaction.response.send_message(f"Successfully kicked {member.mention} for: {reason}")
-    except:
-        await interaction.response.send_message(f"The {member.mention} could not be kicked from the server.", ephemeral=True)
-@kick.error
-async def ban_error(interaction: discord.Interaction, error):
-    if isinstance(error, MissingPermissions):
-        await interaction.response.send_message(f"You don't have permission to use this command!", ephemeral=True)
 
 
 bot.run(TOKEN)
