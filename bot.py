bbdbe6a51b137909a7f77ed21ba6b1be1092f4d1
Kaan <kaan.dnc.7505@gmail.com>
2023-03-21 17:56:06 +0300

    Added Leaderboard command.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import math
 
 #Connecting to database.
 import sqlite3
@@ -21,24 +22,26 @@ TOKEN = os.getenv("TOKEN")
 bot = commands.Bot(command_prefix='!', intents= discord.Intents.all())
 bot.remove_command('help')
 
-#Getting ready for bot.
+# Getting ready for bot.
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
     print('Connected to bot: {}'.format(bot.user.name))
     print('Bot ID: {}'.format(bot.user.id))
     try:
-        sycned = await bot.tree.sync()
-        print(f'Sycned {len(sycned)} global commands.')
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} global commands.')
     except Exception as e:
         print(e)
+
+WORDS = ("ty", "thanks", "thank you")
 
 #Checking every message for is there any words that we want to detect.
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    elif message.content.lower() in ['thanks', 'ty']:
+    elif any(word in message.content.lower() for word in WORDS):
         cursor = conn.cursor()
         cursor.execute('SELECT xp, level FROM users WHERE id = ?', (message.author.id,))
         result = cursor.fetchone()
@@ -48,13 +51,26 @@ async def on_message(message):
         else:
             xp, level = result
             xp += 1
-            if xp >= level * 10:
+            max_level_xp = level * 2 * 1.2
+            max_level_xp = round(max_level_xp)
+            if max_level_xp > 1000:
+                max_level_xp = 1000
+            elif max_level_xp < 10:
+                max_level_xp = 10
+            while xp >= max_level_xp:
                 level += 1
+                xp = xp - max_level_xp
             cursor.execute('UPDATE users SET xp = ?, level = ? WHERE id = ?', (xp, level, message.author.id))
             conn.commit()
     else:
         await bot.process_commands(message)
-
+"""
+@bot.command()
+async def xp2(ctx):
+    c.execute('SELECT * FROM users WHERE id = ?', (852585812792836137,))
+    c.execute('UPDATE users SET xp = ?, level = ? WHERE id = ?', (5, 27, 852585812792836137))
+    conn.commit()
+"""
 #User's XP and Level info.
 @bot.command()
 async def xp(ctx):
@@ -66,5 +82,37 @@ async def xp(ctx):
         xp = result[3]
         level = result[2]
         await ctx.send(f'Your level is {level} with {xp} XP.')
+
+#Leaderboard command
+@bot.command()
+async def leaderboard(ctx):
+    check = False
+    embed = discord.Embed(title="Leaderboard", description="Top 5 Users by Level and XP", color=discord.Color.blue())
+    c.execute('SELECT name, level, xp FROM users ORDER BY level DESC, xp DESC LIMIT 5')
+    result = c.fetchall()
+    i = 1
+    for row in result:
+        name = row[0]
+        level = row[1]
+        xp = row[2]
+        if str(name) == str(ctx.author):
+            embed.add_field(name=f"{i}. {name} (You)", value=f"Level: {level}\nXP: {xp}", inline=False)
+            check = True
+        else:
+            embed.add_field(name=f"{i}. {name}", value=f"Level: {level}\nXP: {xp}", inline=False)
+        i += 1
+    c.execute('SELECT name, level, xp FROM users WHERE id = ?', (ctx.author.id,))
+    result = c.fetchone()
+    if result is not None:
+        name = result[0]
+        level = result[1]
+        xp = result[2]
+        c.execute('SELECT COUNT(*) FROM users WHERE level > ? OR (level = ? AND xp > ?)', (level, level, xp))
+        rank = c.fetchone()[0] + 1
+        if check == False:
+            embed.add_field(name=f"{name} (You)", value=f"Level: {level}\nXP: {xp}\nRank: {rank}", inline=False)
+    else:
+        embed.add_field(name=f"{ctx.author} (You)", value=f"You don't have any XP and Level.", inline=False)
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
