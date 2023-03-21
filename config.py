ae71edebe5665b65a20f2c0f1eb5c28fe65e5b98
Kaan <kaan.dnc.7505@gmail.com>
2023-03-21 18:45:56 +0300

    Created config file.

---
+++ config.py
@@ -0,0 +1,10 @@
+WORDS = ("ty", "thanks", "thank you") # Words needed to gain XP.
+level_xp_multiplier = 1.2 # The number that multiplies the xp required when leveling up.
+max_level = 500 # Maximum reachable level.
+min_level_experience = 10 # Minimum required xp to level up.
+max_level_experience = 1000 # Maximum required xp to level up.
+
+admin_ids = (
+    "124609088108625920", # respuNN
+    "174100874522525697" # Eslorex
+)
